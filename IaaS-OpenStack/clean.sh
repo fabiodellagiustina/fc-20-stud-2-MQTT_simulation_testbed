4e45f088c42e747a3d0e81deaf0516960c282b99
fabiodellagiustina <fabiodellagiustina1997@gmail.com>
2020-05-14 10:17:28 +0200

    Tidy up

--- IaaS-OpenStack/clean.sh
+++ IaaS-OpenStack/clean.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+echo 'Cleaning network, router and instances as "project_admin"'
 ansible-playbook pb-clean-project-admin.yml
+
+echo 'Cleaning project, users, images and flavors as "admin"'
 ansible-playbook pb-clean-admin.yml
--- IaaS-OpenStack/deploy.sh
+++ IaaS-OpenStack/deploy.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+
+echo 'Deploying project, users, images and flavors as "admin"'
 ansible-playbook pb-deploy-admin.yml
+
+echo 'Allocating floating IP "172.24.4.100" to project "project"'
+openstack --os-cloud admin floating ip create \
+  --floating-ip-address 172.24.4.100 \
+  --description "Web server" \
+  --project project \
+  public
+
+echo 'Allocating floating IP "172.24.4.101" to project "project"'
+openstack --os-cloud admin floating ip create \
+  --floating-ip-address 172.24.4.101 \
+  --description "MQTT broker" \
+  --project project \
+  public
+
+echo 'Deploying network, router and instances as "project_admin"'
 ansible-playbook pb-deploy-project-admin.yml
