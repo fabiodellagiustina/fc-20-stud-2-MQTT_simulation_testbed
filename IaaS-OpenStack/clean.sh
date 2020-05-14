7bdfce4ae70f2c86c068ec10356d9f0fff6771fc
fabiodellagiustina <fabiodellagiustina1997@gmail.com>
2020-05-14 12:31:27 +0200

    Delete ping, web, mqtt sec groups

--- IaaS-OpenStack/clean.sh
+++ IaaS-OpenStack/clean.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo 'Cleaning network, router and instances as "project_admin"'
+echo 'Cleaning network, router, security groups and instances as "project_admin"'
 ansible-playbook pb-clean-project-admin.yml
 
-echo 'Deleting floating IP "172.24.4.100"'
+echo 'Releasing floating IP "172.24.4.100"'
 openstack --os-cloud admin floating ip delete 172.24.4.100
 
-echo 'Deleting floating IP "172.24.4.101"'
+echo 'Releasing floating IP "172.24.4.101"'
 openstack --os-cloud admin floating ip delete 172.24.4.101
 
 echo 'Cleaning project, users, images and flavors as "admin"'
