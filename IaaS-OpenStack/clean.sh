a5acf235f1513398d172d334f65e2a625565d6a5
fabiodellagiustina <fabiodellagiustina1997@gmail.com>
2020-05-14 10:55:42 +0200

    Delete fip

--- IaaS-OpenStack/clean.sh
+++ IaaS-OpenStack/clean.sh
@@ -3,5 +3,11 @@
 echo 'Cleaning network, router and instances as "project_admin"'
 ansible-playbook pb-clean-project-admin.yml
 
+echo 'Deleting floating IP "172.24.4.100"'
+openstack --os-cloud admin floating ip delete 172.24.4.100
+
+echo 'Deleting floating IP "172.24.4.101"'
+openstack --os-cloud admin floating ip delete 172.24.4.101
+
 echo 'Cleaning project, users, images and flavors as "admin"'
 ansible-playbook pb-clean-admin.yml
